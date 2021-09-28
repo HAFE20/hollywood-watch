3e3df4d0a688276fd247f26f9670c634a062055d
David Andersson <david.andersson@zocom.se>
2021-09-28 16:51:49 +0200

    Mockup från CodePen, inlagd i React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Exempel mockup:
+// https://codepen.io/david-zocom/pen/ExXGOXd?editors=1100
+const App = () => (
+	<>
+		<header>
+			<h1> Hollywood watch </h1>
+			<input type="text" />
+			<span className="magnify-search">🔍</span>
+		</header>
+
+		<main className="card-grid">
+			<section className="card">
+				<div className="image"> image </div>
+				<h3> My neighbour Totoro </h3>
+				<p> 2021-09-28 </p>
+			</section>
+
+			<section className="card">
+				<div className="image"> image </div>
+				<h3> My neighbour Totoro </h3>
+				<p> 2021-09-28 </p>
+			</section>
+
+			<section className="card">
+				<div className="image"> image </div>
+				<h3> My neighbour Totoro </h3>
+				<p> 2021-09-28 </p>
+			</section>
+
+			<section className="card">
+				<div className="image"> image </div>
+				<h3> My neighbour Totoro </h3>
+				<p> 2021-09-28 </p>
+			</section>
+
+			<section className="card">
+				<div className="image"> image </div>
+				<h3> My neighbour Totoro </h3>
+				<p> 2021-09-28 </p>
+			</section>
+
+			<section className="card">
+				<div className="image"> image </div>
+				<h3> My neighbour Totoro </h3>
+				<p> 2021-09-28 </p>
+			</section>
+		</main>
+	</>
+);
+
 
 export default App;
