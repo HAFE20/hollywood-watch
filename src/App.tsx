c8fc66f6d747e2cd3b0382b65c21794df4e7edaf
David Andersson <david.andersson@zocom.se>
2021-10-05 12:10:20 +0200

    Added data and typescript interface to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-// import React from 'react';
+import { useState } from 'react';
+import { Movie } from './models/Movie'
 import './App.css';
 
 // Exempel mockup:
 // https://codepen.io/david-zocom/pen/ExXGOXd?editors=1100
-const App = () => (
+
+
+const data: Movie[] = [
+	{
+		id: '123', title: 'Jungle cat', premiere: 1960, imgSrc: 'jungle-cat.jpg'
+	},
+	{
+		id: '124', title: 'Two mugs from Brooklyn', premiere: 1942, imgSrc: 'two-mugs-from-brooklyn.jpg'
+	}
+]
+
+const App = () => {
+	const [movies/*, setMovies*/] = useState<Movie[]>(data)
+
+	return (
 	<>
 		<header>
 			<h1> Hollywood watch </h1>
@@ -12,44 +27,18 @@ const App = () => (
 		</header>
 
 		<main className="card-grid">
-			<section className="card">
-				<div className="image"> image </div>
-				<h3> My neighbour Totoro </h3>
-				<p> 2021-09-28 </p>
-			</section>
-
-			<section className="card">
-				<div className="image"> image </div>
-				<h3> My neighbour Totoro </h3>
-				<p> 2021-09-28 </p>
-			</section>
-
-			<section className="card">
-				<div className="image"> image </div>
-				<h3> My neighbour Totoro </h3>
-				<p> 2021-09-28 </p>
-			</section>
-
-			<section className="card">
-				<div className="image"> image </div>
-				<h3> My neighbour Totoro </h3>
-				<p> 2021-09-28 </p>
-			</section>
-
-			<section className="card">
-				<div className="image"> image </div>
-				<h3> My neighbour Totoro </h3>
-				<p> 2021-09-28 </p>
-			</section>
-
-			<section className="card">
-				<div className="image"> image </div>
-				<h3> My neighbour Totoro </h3>
-				<p> 2021-09-28 </p>
-			</section>
+			{movies.map(movie => (
+				<section key={movie.id} className="card">
+					<div className="image">
+						<img src={'./assets/' + movie.imgSrc} alt="Movie poster" />
+					</div>
+					<h3> {movie.title} </h3>
+					<p> {movie.premiere} </p>
+				</section>
+			))}
 		</main>
 	</>
-);
+)};
 
 
 export default App;
---
+++ src/models/Movie.ts
@@ -0,0 +1,6 @@
+export interface Movie {
+	id: string;
+	title: string;
+	imgSrc: string;
+	premiere: number;
+}
