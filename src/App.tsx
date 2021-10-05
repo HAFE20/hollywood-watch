38465794805f0ddf38aa07a333c9c4738dc44849
David Andersson <david.andersson@zocom.se>
2021-10-05 12:16:28 +0200

    Added more data

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,24 @@ const data: Movie[] = [
 	},
 	{
 		id: '124', title: 'Two mugs from Brooklyn', premiere: 1942, imgSrc: 'two-mugs-from-brooklyn.jpg'
+	},
+	{
+		id: '125', title: 'Case of the Red Monkey', premiere: 1955, imgSrc: 'case-of-the-red-monkey.jpg'
+	},
+	{
+		id: '126', title: 'Stop Me Before I Kill!', premiere: 1960, imgSrc: 'stop-me-before-i-kill.jpg'
+	},
+	{
+		id: '127', title: 'The End of the Line', premiere: 1957, imgSrc: 'the-end-of-the-line.jpg'
+	},
+	{
+		id: '128', title: 'Night of the Bloody Apes; Feast of Flesh', premiere: 1969, imgSrc: 'night-of-the-bloody-apes.jpg'
+	},
+	{
+		id: '129', title: 'Savage!', premiere: 1962, imgSrc: 'savage!.jpg'
+	},
+	{
+		id: '130', title: 'Underworld, U.S.A.', premiere: 1961, imgSrc: 'underworld-usa.jpg'
 	}
 ]
 
